df9dfffc38efa294c6743ec7168db654135aabf8
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-27 13:15:48 -0300

    Feature: implementa metodos na interface

--- src/DEVinCer.Domain/Interfaces/Service/IStateService.cs
+++ src/DEVinCer.Domain/Interfaces/Service/IStateService.cs
@@ -1,5 +1,14 @@
+using DEVinCar.Api.DTOs;
+using DEVinCar.Api.ViewModels;
+
 namespace DEVinCer.Domain.Interfaces.Service;
 
 public interface IStateService
 {
+    IList<GetStateViewModel> ListAll();
+    GetStateByIdViewModel GetStateByID(int stateId);
+    GetCityByIdViewModel GetCityById(int cityId);
+    void InsertCity(int stateId, CityDTO city);
+    void InsertAdress(int stateId, int ciryId, AdressDTO adress);
+
 }
