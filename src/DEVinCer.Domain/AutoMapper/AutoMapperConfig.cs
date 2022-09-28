9cd5973caa21cdeaf901543bb8ed57064822d67c
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-28 14:21:24 -0300

    Feature: Cria classe com configuracao do autoMapper

---
+++ src/DEVinCer.Domain/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+
+namespace DEVinCer.Domain.AutoMapper;
+
+public class AutoMapperConfig
+{
+    public static IMapper Configure()
+    {
+        var ConfigMap = new MapperConfiguration( config => {
+            config.AddProfile(new EntityAutoMapper());
+        });
+
+        return ConfigMap.CreateMapper();
+        
+    }
+}
