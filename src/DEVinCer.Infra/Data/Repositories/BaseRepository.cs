0a0fa7bd11260ea38457246d9ce1e3e8b1e2bf24
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-27 22:30:19 -0300

    Feature: Cria repositorio base

--- src/DEVinCer.Infra/Data/Repositories/BaseRepository.cs
+++ src/DEVinCer.Infra/Data/Repositories/BaseRepository.cs
@@ -1,5 +1,41 @@
+using DEVinCar.Infra.Data;
+
 namespace DEVinCer.Infra.Data.Repositories;
 
-public class BaseRepository
+public class BaseRepository <TEntity, Tkey> where TEntity : class
 {
+    private readonly DevInCarDbContext _context;
+
+    public BaseRepository(DevInCarDbContext context)
+    {
+        _context = context;
+    }
+
+    public virtual void Update(TEntity entity)
+    {
+        _context.Set<TEntity>().Update(entity);
+        _context.SaveChanges();
+    }
+
+    public virtual void Delete(TEntity entity)
+    {
+        _context.Set<TEntity>().Remove(entity);
+        _context.SaveChanges();
+    }
+
+    public virtual void Insert(TEntity entity)
+    {
+        _context.Set<TEntity>().Add(entity);
+        _context.SaveChanges();
+    }
+
+    public virtual IList<TEntity> ListAll()
+    {
+        return _context.Set<TEntity>().ToList();
+    }
+
+    public virtual TEntity GetById(Tkey id)
+    {
+        return _context.Set<TEntity>().Find(id);
+    }
 }
