6e796b41572df322208135845d9ef69a3a9f0bf9
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-28 13:05:50 -0300

    fix: implemente permissao nos users

--- src/DEVinCer.Infra/Data/Mappings/UserMap.cs
+++ src/DEVinCer.Infra/Data/Mappings/UserMap.cs
@@ -1,4 +1,5 @@
 using DEVinCar.Domain.Models;
+using DEVinCer.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -38,10 +39,10 @@ public class UserMap : IEntityTypeConfiguration<User>
             .HasColumnType("DATE");
 
         entity.HasData(new[] {
-                new User (1, "jose@email.com", "123456opp78", "Jose", new DateTime(2000, 12, 10)),
-                new User (2, "andrea@email.com", "987dasd654321", "Andrea", new DateTime(1999, 05, 11)),
-                new User (3, "adao@email.com", "2589asd", "Adao", new DateTime(2005, 09, 02)),
-                new User (4, "monique@email.com", "asd45uio", "Monique", new DateTime(2001, 06, 07)),
+                new User (1, "jose@email.com", "123456opp78", "Jose", new DateTime(2000, 12, 10), Roles.Manager),
+                new User (2, "andrea@email.com", "987dasd654321", "Andrea", new DateTime(1999, 05, 11), Roles.Seller),
+                new User (3, "adao@email.com", "2589asd", "Adao", new DateTime(2005, 09, 02), Roles.Buyer),
+                new User (4, "monique@email.com", "asd45uio", "Monique", new DateTime(2001, 06, 07), Roles.Buyer),
             });
     }
 
