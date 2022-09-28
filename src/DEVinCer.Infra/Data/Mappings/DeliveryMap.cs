443999f82b1e7f7042a858e3cf0affd8de105f99
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-28 11:04:59 -0300

    Feature: mapeia entidade delivery

--- src/DEVinCer.Infra/Data/Mappings/DeliveryMap.cs
+++ src/DEVinCer.Infra/Data/Mappings/DeliveryMap.cs
@@ -1,5 +1,40 @@
+using DEVinCar.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace DEVinCer.Infra.Data.Mappings;
 
-public class DeliveryMap
+public class DeliveryMap : IEntityTypeConfiguration<Delivery>
 {
+    public void Configure(EntityTypeBuilder<Delivery> entity)
+    {
+        entity.ToTable("DELIVERIES");
+
+        entity.HasKey(d => d.Id);
+
+        entity.Property(d => d.Id)
+            .HasColumnName("ID")
+            .HasColumnType("uniqueidentifier");
+
+        entity.Property(d => d.AddressId)
+            .HasColumnName("ADDRESS_ID")
+            .HasColumnType("int");
+
+        entity.Property(d => d.SaleId)
+            .HasColumnName("SALE_ID")
+            .HasColumnType("int");
+
+        entity.Property(d => d.DeliveryForecast)
+            .HasColumnName("DELIVERY_FORECAST")
+            .HasColumnType("DATE");
+            
+
+        entity.HasOne<Address>(d => d.Address)
+            .WithMany(a => a.Deliveries)
+            .HasForeignKey(d => d.AddressId);
+        
+        entity.HasOne<Sale>(d => d.Sale)
+            .WithMany(s => s.Deliveries)
+            .HasForeignKey(d => d.SaleId);
+    }
 }
