0f4ae70db893300833f9fb210827f3a6942c524f
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-28 10:56:01 -0300

    Feature: mapeia entidade city

--- src/DEVinCer.Infra/Data/Mappings/CityMap.cs
+++ src/DEVinCer.Infra/Data/Mappings/CityMap.cs
@@ -1,5 +1,36 @@
+using DEVinCar.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace DEVinCer.Infra.Data.Mappings;
 
-public class CityMap
+public class CityMap : IEntityTypeConfiguration<City>
 {
+    public void Configure(EntityTypeBuilder<City> entity)
+    {
+        entity.ToTable("CITIES");
+
+        entity.HasKey(c => c.Id);
+
+        entity.Property(c => c.Id)
+            .HasColumnName("ID")
+            .HasColumnType("uniqueidentifier");
+
+        entity.Property(c => c.Name)
+            .HasColumnName("NAME")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(200)
+            .IsRequired();
+
+        entity.Property(c => c.StateId)
+            .HasColumnName("STATE_ID")
+            .HasColumnType("int")
+            .IsRequired();
+
+        entity.HasOne<State>(c => c.State)
+            .WithMany(s => s.Cities)
+            .HasForeignKey(c => c.StateId)
+            .IsRequired();
+    }
+
 }
