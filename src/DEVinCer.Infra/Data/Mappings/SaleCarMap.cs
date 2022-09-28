b2589ef408b8c82e071810d7826098d5b730d887
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-28 11:11:59 -0300

    Feature: Mapeia entidade saleCar

--- src/DEVinCer.Infra/Data/Mappings/SaleCarMap.cs
+++ src/DEVinCer.Infra/Data/Mappings/SaleCarMap.cs
@@ -1,5 +1,46 @@
+using DEVinCar.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace DEVinCer.Infra.Data.Mappings;
 
-public class SaleCarMap
+public class SaleCarMap : IEntityTypeConfiguration<SaleCar>
 {
+    public void Configure(EntityTypeBuilder<SaleCar> entity)
+    {
+        entity.ToTable("SALECAR");
+
+        entity.HasKey(sc => sc.Id);
+
+        entity.Property(sc => sc.Id)
+            .HasColumnName("ID")
+            .HasColumnType("uniqueidentifier");
+
+        entity.Property(sc => sc.SaleId)
+            .HasColumnName("SALE_ID")
+            .HasColumnType("int")
+            .IsRequired();
+
+        entity.Property(sc => sc.CarId)
+            .HasColumnName("CAR_ID")
+            .HasColumnType("int")
+            .IsRequired();
+
+        entity.Property(sc => sc.UnitPrice)
+            .HasColumnName("UNIT_PRICE")
+            .HasPrecision(18, 2);
+
+        entity.Property(sc => sc.Amount)
+            .HasColumnName("AMOUNT")
+            .HasColumnType("int");
+
+        entity.HasOne<Car>(sc => sc.Car)
+            .WithMany(c => c.Sales)
+            .HasForeignKey(sc => sc.Id);                
+
+        entity.HasOne<Sale>(sc => sc.Sale)
+            .WithMany(s => s.Cars)
+            .HasForeignKey(sc => sc.Id);    
+    }
+
 }
