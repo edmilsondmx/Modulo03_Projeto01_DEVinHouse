06716e54b9e0eda17beff017c9d3028ac4eb7134
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-29 18:37:42 -0300

    Feature: implementa metodos para transferencia entre controller e repository

--- src/DEVinCer.Domain/Services/UserService.cs
+++ src/DEVinCer.Domain/Services/UserService.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using DEVinCar.Domain.DTOs;
+using DEVinCar.Domain.Models;
 using DEVinCer.Domain.Interfaces.Repository;
 using DEVinCer.Domain.Interfaces.Service;
 
@@ -18,37 +19,89 @@ public class UserService : IUserService
 
     public void DeleteUser(int id)
     {
-        throw new NotImplementedException();
+        var userDb = _userRepository.GetById(id);
+        if(userDb == null)
+            throw new Exception("Não existe registro!");
+            //TODO: Criar exception para lista vazia!
+        
+        _userRepository.Delete(userDb);
+
     }
 
-    public IList<SaleCarDTO> GetBuyerByUserID(int id)
+    public IList<SaleDTO> GetBuyerByUserID(int id)
     {
-        throw new NotImplementedException();
+        var buyerByUserId = _userRepository.GetBuyerByUserID(id);
+        if(!buyerByUserId.ToList().Any())
+            throw new Exception("Não existe registro!");
+            //TODO: Criar exception para lista vazia!
+        
+        return _mapper.Map<IList<SaleDTO>>(buyerByUserId);
     }
 
     public UserDTO GetById(int id)
     {
-        throw new NotImplementedException();
+        var userDb = _userRepository.GetById(id);
+
+        if(userDb == null)
+            throw new Exception("Não existe registro!");
+            //TODO: Criar exception para lista vazia!
+        
+        return _mapper.Map<UserDTO>(userDb);
     }
 
-    public IList<SaleCarDTO> GetSalesByUserID(int id)
+    public IList<SaleDTO> GetSalesByUserID(int id)
     {
-        throw new NotImplementedException();
+        var salesByUserId = _userRepository.GetSalesByUserID(id);
+        if(!salesByUserId.ToList().Any())
+            throw new Exception("Não existe registro!");
+            //TODO: Criar exception para lista vazia!
+        
+        return _mapper.Map<IList<SaleDTO>>(salesByUserId);
     }
 
     public void Insert(UserDTO dto)
     {
-        throw new NotImplementedException();
+        if(IsExists(dto))
+            throw new Exception("Usuario já cadastrado");
+            //TODO: Criar exception usuario já cadastrado!
+        
+        _userRepository.Insert(_mapper.Map<User>(dto));
     }
 
     public void InsertBuy(int userId, BuyDTO dto)
     {
-        throw new NotImplementedException();
+        var userDb = _userRepository.GetById(userId);
+        var seller = _userRepository.GetById(dto.SellerId);
+
+        if(!IsExists(_mapper.Map<UserDTO>(userDb)))
+            throw new Exception("Usuario não encontrado!");
+            //TODO: Criar exception usuario já cadastrado!
+
+        if(!IsExists(_mapper.Map<UserDTO>(seller)))
+            throw new Exception("Comprador não encontrado!");
+            //TODO: Criar exception usuario já cadastrado!
+        
+        Sale buy = _mapper.Map<Sale>(dto);
+        buy.BuyerId = userId;
+
+        _userRepository.InsertSale(buy);
     }
 
     public void InsertSale(int userId, SaleDTO dto)
     {
-        throw new NotImplementedException();
+        var userDb = _userRepository.GetById(userId);
+        var buyer = _userRepository.GetById(dto.BuyerId);
+
+        if(!IsExists(_mapper.Map<UserDTO>(userDb)))
+            throw new Exception("Usuario não encontrado!");
+            //TODO: Criar exception usuario já cadastrado!
+
+        if(!IsExists(_mapper.Map<UserDTO>(buyer)))
+            throw new Exception("Comprador não encontrado!");
+            //TODO: Criar exception usuario já cadastrado!
+        
+        dto.SellerId = userId;
+        _userRepository.InsertSale(_mapper.Map<Sale>(dto));
     }
 
     public IList<UserDTO> ListAll(string name, DateTime? birthDateMax, DateTime? birthDateMin)
@@ -74,5 +127,12 @@ public class UserService : IUserService
     public IList<UserDTO> ListAll()
     {
         throw new NotImplementedException();
+    }
+
+    private bool IsExists(UserDTO user)
+    {
+        return _userRepository.ListAll()
+            .Any(u => u.Email == user.Email);
+        
     }
 }
