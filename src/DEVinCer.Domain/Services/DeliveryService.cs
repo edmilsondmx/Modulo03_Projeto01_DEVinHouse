6382579f402a0fc136c7db8b81ec79270bb98b94
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-29 20:16:55 -0300

    feature: implementa metodos no service

--- src/DEVinCer.Domain/Services/DeliveryService.cs
+++ src/DEVinCer.Domain/Services/DeliveryService.cs
@@ -1,12 +1,34 @@
+using AutoMapper;
 using DEVinCar.Domain.DTOs;
+using DEVinCer.Domain.Interfaces.Repository;
 using DEVinCer.Domain.Interfaces.Service;
 
 namespace DEVinCer.Domain.Services;
 
 public class DeliveryService : IDeliveryService
 {
-    public IList<DeliveryDTO> ListAll()
+    private readonly IDeliveryRepository _deliveryRepository;
+    private readonly IMapper _mapper;
+
+    public DeliveryService(IDeliveryRepository deliveryRepository, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _deliveryRepository = deliveryRepository;
+        _mapper = mapper;
+    }
+
+    public IList<DeliveryDTO> ListAll(int? addressId, int? saleId)
+    {
+        var query = _deliveryRepository.ListAll().AsQueryable();
+
+        if(addressId.HasValue)
+            query = query.Where(a => a.AddressId == addressId);
+        
+        if (saleId.HasValue)
+            query = query.Where(s => s.SaleId == saleId);
+        
+        if (!query.ToList().Any())
+            throw new Exception("Não existe registro!");
+
+        return _mapper.Map<IList<DeliveryDTO>>(query).ToList();
     }
 }
