8922c937bec945e6acdc66c6d72d22ed691808fd
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-02 11:45:57 -0300

    feature: cria middleware para fitrar erros

---
+++ src/DEVinCar.Api/Config/ErrorMiddleware.cs
@@ -0,0 +1,49 @@
+using System.Net;
+using DEVinCer.Domain.DTOs;
+using DEVinCer.Domain.Exceptions;
+
+namespace DEVinCar.Api.Config;
+
+public class ErrorMiddleware
+{
+    private readonly RequestDelegate _next;
+    public ErrorMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await TratamentoExcecao(context, ex);
+        }
+        
+    }
+
+    private Task TratamentoExcecao(HttpContext context, Exception ex)
+    {
+        HttpStatusCode status;
+        string message;
+
+        if(ex is IsExistsException)
+        {
+            status = HttpStatusCode.NotFound;
+            message = ex.Message;
+        }
+        else
+        {
+            status = HttpStatusCode.InternalServerError;
+            message = "An error has occurred, please contact TI!";
+        }
+
+
+        var response = new ErrorDTO(message);
+
+        context.Response.StatusCode = (int)status;
+        return context.Response.WriteAsJsonAsync(response);
+    }
+}
