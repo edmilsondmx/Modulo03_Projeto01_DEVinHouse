2e23c617e299496ebe97eba9c9c12a5783150f48
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-03 14:45:13 -0300

    feature: cria classe estatica com chave secreta

---
+++ src/DEVinCer.Domain/Security/Settings.cs
@@ -0,0 +1,6 @@
+namespace DEVinCer.Domain.Security;
+
+public static class Settings
+{
+    public static readonly string Secret = "09702c0ec938fb2579a856da4810db832a8b29ebdc1d7a483cb7e2b192a421de";
+}
