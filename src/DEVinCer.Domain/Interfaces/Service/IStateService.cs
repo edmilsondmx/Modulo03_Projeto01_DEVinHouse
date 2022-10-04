3472b69ae3019ea0ae5ca12bb5c078f0135fdeb2
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-04 10:43:29 -0300

    fix: altera retorno da interface State

--- src/DEVinCer.Domain/Interfaces/Service/IStateService.cs
+++ src/DEVinCer.Domain/Interfaces/Service/IStateService.cs
@@ -8,7 +8,7 @@ public interface IStateService
     IList<GetStateViewModel> ListAll(string name);
     GetStateByIdViewModel GetStateByID(int stateId);
     GetCityByIdViewModel GetCityById(int stateId, int cityId);
-    IList<GetCityByIdViewModel> GetCitiesByStateId(int stateId, string name);
+    IList<CityDTO> GetCitiesByStateId(int stateId, string name);
     void InsertCity(int stateId, CityDTO city);
     void InsertAdress(int stateId, int ciryId, AdressDTO adress);
 
