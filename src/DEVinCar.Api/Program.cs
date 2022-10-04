9d72397a8810fa7f8b4167e778cfb38b1c3b77fe
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-04 12:16:13 -0300

    feature: adiciona configuração de cache na program

--- src/DEVinCar.Api/Program.cs
+++ src/DEVinCar.Api/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMemoryCache();
+builder.Services.AddScoped(typeof(CacheService<>));
+
 builder.Services.AddSingleton(AutoMapperConfig.Configure());
 
 builder.Services.RegisterAuthentication();
