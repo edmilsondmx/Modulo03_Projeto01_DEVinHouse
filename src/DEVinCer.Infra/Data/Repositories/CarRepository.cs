f9165cc564aa1462d12fd0305097fed9afcea61c
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-05 13:07:37 -0300

    fix: implementa metodo de obter lista com paginacao no repositorio car

--- src/DEVinCer.Infra/Data/Repositories/CarRepository.cs
+++ src/DEVinCer.Infra/Data/Repositories/CarRepository.cs
@@ -1,6 +1,7 @@
 using DEVinCar.Domain.Models;
 using DEVinCar.Infra.Data;
 using DEVinCer.Domain.Interfaces.Repository;
+using DEVinCer.Domain.Models;
 
 namespace DEVinCer.Infra.Data.Repositories;
 
@@ -8,5 +9,9 @@ public class CarRepository : BaseRepository<Car, int>, ICarRepository
 {
     public CarRepository(DevInCarDbContext context) : base (context)
     {
+    }
+    public IQueryable<Car> ListAllPg(Pagination pagination)
+    {
+        return _context.Cars.Take(pagination.Take).Skip(pagination.Skip);
     }
 }
