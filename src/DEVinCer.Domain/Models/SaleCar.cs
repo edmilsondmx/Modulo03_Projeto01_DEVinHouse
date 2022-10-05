494523ddd810434d030b976e5e4f890f2136bd7a
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-05 11:38:00 -0300

    feature: implementa retorno com lista de itens

--- src/DEVinCer.Domain/Models/SaleCar.cs
+++ src/DEVinCer.Domain/Models/SaleCar.cs
@@ -3,9 +3,9 @@ namespace DEVinCar.Domain.Models;
 public class SaleCar
 {
     public int Id { get; set; }
+    public int CarId { get; set; }
     public decimal UnitPrice { get; set; }
     public int? Amount { get; set; }
-    public int CarId { get; set; }
     public int SaleId { get; set; }
     public virtual Car Car { get; set; }
     public virtual Sale Sale { get; set; }
