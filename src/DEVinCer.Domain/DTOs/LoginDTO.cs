930e1426f06c32f7dab8db8aa29ef870381de44c
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-06 14:17:04 -0300

    feature: adiciona dataAnnotations em login

--- src/DEVinCer.Domain/DTOs/LoginDTO.cs
+++ src/DEVinCer.Domain/DTOs/LoginDTO.cs
@@ -1,7 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using DEVinCer.Domain.Annotations;
+
 namespace DEVinCer.Domain.DTOs;
 
 public class LoginDTO
 {
+    [Required(ErrorMessage = "The email is required")]
+    [MaxLength(150)]
+    [EmailAddress(ErrorMessage = "Email must be valid")]
     public string Email { get; set; }
+    [Required(ErrorMessage = "The password is required")]
+    [MaxLength(50)]
+    [MinLength(4, ErrorMessage = "The password must contain at least 4 digits")]
     public string Password { get; set; }
 }
